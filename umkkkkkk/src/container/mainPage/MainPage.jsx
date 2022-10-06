f9cab0d16a349fb88b88c1756eb99503fa29d859
ant1pov-a <57625701+ant1pov-a@users.noreply.github.com>
2022-10-06 12:42:00 +0300

    comit++

--- umkkkkkk/src/container/mainPage/MainPage.jsx
+++ umkkkkkk/src/container/mainPage/MainPage.jsx
@@ -4,9 +4,9 @@ class MainPage extends Component {
   render() {
     return (
       <>
-        <h1>Hello</h1>
-        <h2>Hello this is H2</h2>
-        <p>Create About us</p>
+        <h1>Hello++</h1>
+        <h2>Hello this is H2++</h2>
+        <p>Create About us++</p>
       </>
     );
   }
