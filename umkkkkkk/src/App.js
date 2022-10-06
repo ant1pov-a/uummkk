6ce518946229c47a530bedd3a042f554f53a2008
iyuragoloborodko <114994672+iyuragoloborodko@users.noreply.github.com>
2022-10-06 11:13:50 +0300

    final start commin
    final start commin

--- umkkkkkk/src/App.js
+++ umkkkkkk/src/App.js
@@ -1,23 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import mainPage from "./container/mainPage/mainPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Anton gay and pidoraseiu</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <mainPage />;
+  }
 }
 
 export default App;
---
+++ umkkkkkk/src/container/mainPage/mainPage.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class mainPage extends Component {
+  state = {};
+  render() {
+    return "Hello";
+  }
+}
+
+export default mainPage;
