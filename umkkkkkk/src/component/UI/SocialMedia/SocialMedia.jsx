f4d616aba48635ac8c80fcc83a7161569b9afe4a
iyuragoloborodko <114994672+iyuragoloborodko@users.noreply.github.com>
2022-10-13 12:55:21 +0300

    commit

--- umkkkkkk/src/component/UI/SocialMedia/SocialMedia.jsx
+++ umkkkkkk/src/component/UI/SocialMedia/SocialMedia.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { Component } from "react";
+import classes from "./SocialMedia.module.css";
+import facebook from "./../../../img/facebook.png";
+import twitter from "./../../../img/twitter.png";
+import instagram from "./../../../img/instagram.png";
+
 class SocialMedia extends Component {
-    render() { 
-        return ();
-    }
+  render() {
+    return (
+      <ul className={classes.SocialMedia}>
+        <li className={classes.item}>
+          <a className={classes.link} href="#">
+            <img src={facebook} alt="" />
+          </a>
+        </li>
+        <li className={classes.item}>
+          <a className={classes.link} href="#">
+            <img src={twitter} alt="" />
+          </a>
+        </li>
+        <li className={classes.item}>
+          <a className={classes.link} href="#">
+            <img src={instagram} alt="" />
+          </a>
+        </li>
+      </ul>
+    );
+  }
 }
- 
+
 export default SocialMedia;
